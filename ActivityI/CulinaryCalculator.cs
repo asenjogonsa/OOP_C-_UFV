dda3335facf39100c1eee1a2bdd22e158bf1de7f
XDroneX <pablo.asenjo.gonzalez@gmail.com>
2024-02-25 19:15:02 +0100

    :)

---
+++ ActivityI/CulinaryCalculator.cs
@@ -0,0 +1,109 @@
+﻿/*
+Pablo Asenjo
+18/02/2024
+               _ _       \ \
+    .-"""""-. / \_> /\    |/
+   /         \.'`  `',.--//
+ -(           I      I  @@\
+   \         /'.____.'\___|
+    '-.....-' __/ | \   (`)
+             /   /  /
+                 \  \
+*/
+
+
+
+using System;
+
+namespace oppguidedpw
+{
+    public class Program
+    {
+
+        //empezamos con las funciones
+        public static int Add(int op1, int op2)
+        {
+            return op1 + op2;
+        }
+        public static int Subtract(int op1, int op2)
+        {
+            return op1 - op2;
+        }
+        public static int Multiply(int op1, int op2)
+        {
+            return op1 * op2;
+        }
+        public static int Divide(int op1, int op2)
+        {
+            return op1 / op2;
+        }
+        public static void Main()
+        {
+            int option = 1;      //sera usado para asignar la operacion a realizar
+            int result = 0;      //se le asignara el valor final
+
+            string tmp = "";
+            string sign = "";    //para el simbolo "lit"
+
+            while (option >= 1 && option <= 5)
+            {
+                Console.WriteLine("**************************");
+                Console.WriteLine("*     1. Add             *");
+                Console.WriteLine("*     2. Subtract        *");
+                Console.WriteLine("*     3. Multiply        *");
+                Console.WriteLine("*     4. Divide          *");
+                Console.WriteLine("*     5. Exit            *");
+                Console.WriteLine("**************************");
+                
+                tmp = Console.ReadLine();
+                option = int.Parse(tmp);
+
+               
+
+                if (option >= 5) 
+                {
+                    break;
+                }
+
+                Console.WriteLine("Insert the value of the fist operator: ");
+                int op1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Insert the value of the second operator: ");
+                int op2 = int.Parse(Console.ReadLine());
+
+                if (option == 1)         //sum
+                {
+                    result = Add(op1, op2);
+                    sign = " + ";
+                }
+                else if (option == 2)    //resta
+                {
+                    result = Subtract(op1, op2);
+                    sign = " - ";
+                }
+                else if (option == 3)    //mult
+                {
+                    result = Multiply(op1, op2);
+                    sign = " * ";
+                }
+                else if (option == 4)    //divi
+                {
+
+                    if (op2 == 0) // por si
+                    {
+                            Console.WriteLine("It´s imposible to perform te opertation");
+                    }
+                    else 
+                    {
+                        result = Divide(op1, op2);
+                        sign = " / ";
+                    }
+                }
+
+                Console.WriteLine(op1 + sign + op2 + " = " + result);
+
+            }
+        }
+    }
+}
+
---
+++ ActivityII/CulinaryCalculatorII.cs
@@ -0,0 +1,154 @@
+﻿/*
+Pablo Asenjo
+25/02/2024
+               _ _       \ \
+    .-"""""-. / \_> /\    |/
+   /         \.'`  `',.--//
+ -(           I      I  @@\
+   \         /'.____.'\___|
+    '-.....-' __/ | \   (`)
+             /   /  /
+                 \  \
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace code
+{
+    public class Calculator
+    {
+        private const int EXIT = 7;
+        public Calculator()
+        {
+
+        }
+        public int PrintOperation()
+        {
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine(" 1. Add");
+            Console.WriteLine(" 2. Subtract");
+            Console.WriteLine(" 3. Multiply");
+            Console.WriteLine(" 4. Divide");
+            Console.WriteLine(" 5. Pow");
+            Console.WriteLine(" 6. Modules");
+            Console.WriteLine(" 7. Exit");
+            Console.WriteLine("--------------------------------------");
+
+            int option = int.Parse(Console.ReadLine());
+
+            if (option < 1 || option > 7 && option == EXIT)
+            {
+                return EXIT;
+            }
+
+            return option;
+        }
+        public static int Add(int op1, int op2)
+        {
+            return op1 + op2;
+        }
+        public static int Subtract(int op1, int op2)
+        {
+            return op1 - op2;
+        }
+        public static int Multiply(int op1, int op2)
+        {
+            return op1 * op2;
+        }
+        public static int Divide(int op1, int op2)
+        {
+            if (op2 == 0)
+                return Int32.MaxValue;
+            else
+                return op1 / op2;
+        }
+        public static int Pow(int op1, int op2)
+        {
+            int resultado = 1;
+            if (op2 == 0)
+            {
+                return 1;
+            }
+            for (int i = 0; i < op2; i++)
+            {
+                resultado *= op1;
+            }
+            return resultado;
+        }
+        public static int Mod(int op1, int op2)
+        {
+            if (op2 == 0)
+            {
+                return 0;
+            }
+            return op1 % op2;
+        }
+        public void Perform_operation(int op1, int op2, int op)
+        {
+            double solucion = 0;
+            string opt = "";
+            if (op == 1)
+            {
+                opt = "+";
+                solucion = Add(op1, op2);
+            }
+            else if (op == 2)
+            {
+                opt = "-";
+                solucion = Subtract(op1, op2);
+            }
+            else if (op == 3)
+            {
+                opt = "*";
+                solucion = Multiply(op1, op2);
+            }
+            else if (op == 4)
+            {
+                opt = "/";
+                solucion = Divide(op1, op2);
+            }
+            else if (op == 5)
+            {
+                opt = "^";
+                solucion = Pow(op1, op2);
+            }
+            else if (op == 6)
+            {
+                opt = "%";
+                solucion = Mod(op1, op2);
+            }
+            Console.WriteLine($"{op1}{opt}{op2}={solucion}");
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Calculator calculator = new Calculator();
+
+            int option = calculator.PrintOperation();
+            while (option >= 1 && option < 7)
+            {
+
+                Console.Clear();
+                Console.Write("Insert the value of the first operator: ");
+                int op1 = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+                Console.Write("Insert the value of the second operator: ");
+                int op2 = int.Parse(Console.ReadLine());
+
+                calculator.Perform_operation(op1, op2, option);
+
+                option = calculator.PrintOperation();
+            }
+            Console.WriteLine("Exiting");
+
+        }
+    }
+}
